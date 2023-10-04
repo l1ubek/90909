197e42ff080c7a7bf6af503998929f9dc63dfbaa
liubomyr stetsiuk <liubomyr.stetsiuk.shi.2023@lpnu.ua>
2023-10-04 18:45:07 +0300

    Насрав

--- 123.java
+++ 123.java
@@ -1,5 +1,27 @@
-publci class main
+
+import java.util.Scanner;
+
+public class 123 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Москаль чи хохол? ");
+        int month = scanner.nextString();
+        if (month = Хохол) {
+            System.out.println("У " + month + " місяці " + month1 + " днів");
+        }
+
+    }
+
+    private static int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public static int days(int month) {
+        if (month >= 1 && month <= 12) {
+            return daysInMonth[month - 1];
+        } else {
+            System.out.println("Ваше значення повинно бути 1-12");
+            return 0;
+        }
+    }
+}
 
 
-909
-o909
